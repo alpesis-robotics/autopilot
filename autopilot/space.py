b464bcde6ffd058e4a015b26598464200500a57e
K <kwailamchan@hotmail.com>
2018-03-18 15:38:45 +0800

    [master] created configurable space. 	new file:   colliders.csv 	new file:   space.py

---
+++ autopilot/space.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.rcParams["figure.figsize"] = [12, 12]
+
+
+def create_grid(data, drone_altitude, safety_distance):
+    """
+    Returns a grid representation of a 2D configuration space 
+    based on given obstacle data, drone altitude and safety distance
+    arguments.
+    """
+
+    # minimum and maximum north coordinates
+    north_min = np.floor(np.amin(data[:, 0] - data[:, 3]))
+    north_max = np.ceil(np.amax(data[:, 0] + data[:, 3]))
+
+    # minimum and maximum east coordinates
+    east_min = np.floor(np.amin(data[:, 1] - data[:, 4]))
+    east_max = np.ceil(np.amax(data[:, 1] + data[:, 4]))
+
+    # given the minimum and maximum coordinates we can 
+    # calculate the size of the grid
+    north_size = int(np.ceil((north_max - north_min)))
+    east_size = int(np.ceil((east_max - east_min)))
+   
+    grid = np.zeros((north_size, east_size))
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+        if alt + d_alt + safety_distance > drone_altitude:
+            obstacle = [
+                int(north - d_north - safety_distance - north_min),
+                int(north + d_north + safety_distance - north_min),
+                int(east - d_east - safety_distance - east_min),
+                int(east + d_east + safety_distance - east_min)
+            ]
+            grid[obstacle[0]:obstacle[1], obstacle[2]:obstacle[3]] = 1        
+
+    return grid
+
+
+if __name__ == '__main__':
+
+    filename = 'colliders.csv'
+    data = np.loadtxt(filename, delimiter=',', dtype="Float64", skiprows=2)
+    print(data)
+
+    # static drone altitude (metres)
+    drone_altitude = 5
+    # minimum distance required to stay away from an obstacle (metres)
+    safe_distance = 3
+
+    grid = create_grid(data, drone_altitude, safe_distance)
+    plt.imshow(grid, origin="lower")
+    plt.xlabel('EAST')
+    plt.ylabel('NORTH')
+    plt.show()
